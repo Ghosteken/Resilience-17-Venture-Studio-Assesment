ae0b7785bbff8d49d69d5210974e3106989d250c
nicholas <aigberuan6@gmail.com>
2026-06-15 20:59:52 +0100

    feat(core): fix and update validator-vsl line processors

--- core/validator-vsl/line-processors/spread-processor.js
+++ core/validator-vsl/line-processors/spread-processor.js
@@ -1,19 +1,18 @@
 const { h, buildRegexFromComplexString } = require('../utils/regex-builder');
-endpointRegexString = h`
+
+const spreadRegexString = h`
 ^
   \s*
   (?:\.{3}(?<SPREAD_PROP>[a-zA-Z$_-]+[a-zA-Z$_0-9-\.]*))
   \s*
 $
 `;
-const endpointRegex = buildRegexFromComplexString(endpointRegexString);
+const spreadRegex = buildRegexFromComplexString(spreadRegexString);
 function spreadLineProcessor(line = '') {
   let nodeInfo = {};
-  const lineMatches = line.match(endpointRegex);
+  const lineMatches = line.match(spreadRegex);
   const lineGroups = lineMatches?.groups;
-  const {
-    SPREAD_PROP,
-  } = lineGroups || {};
+  const { SPREAD_PROP } = lineGroups || {};
   nodeInfo = {
     lineMatched: !!SPREAD_PROP,
     name: SPREAD_PROP,
